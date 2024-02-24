66ea7c00458ecaa1115a3aa31b03fb2f7977a69b
Sudhan1112 <sudhanssudhan83@gmail.com>
2024-02-24 17:17:01 +0530

    Connected to the server

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const { startDatabase, stopDatabase, isConnected } = require('./db')
+require('dotenv').config()
+
 
 app.get('/ping', (req, res) => {
     res.send("Hello World")
 })
 
 app.get('', (req, res) => {
-    res.send('Hello World testing')
+    res.send('Hello World testing' `${isConnected() ? 'connected' : 'Disconnected'}`)
 })
 
 app.use((req, res) => {
