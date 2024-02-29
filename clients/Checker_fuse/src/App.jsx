a9ca72da195894fa6c5658a08aa39b369f67d525
Sudhan1112 <sudhanssudhan83@gmail.com>
2024-02-29 12:05:33 +0530

    About my project

--- clients/Checker_fuse/src/App.jsx
+++ clients/Checker_fuse/src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import React from 'react'
 
 function App() {
 
   return (
-    <>
-      </>
+    <div>
+      <h1>Checker Fuse</h1><br />
+      <p>Checker Fuse is a project aimed at tracking the progression of a set of students listed within an application. It provides real-time check-in and check-out functionalities, contributing to productivity management within educational or work environments.</p>
+    </div>
   )
 }
 
