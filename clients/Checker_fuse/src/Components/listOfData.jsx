4293208ebd3fecd382e4bdbb760525b6bd1854db
Sudhan1112 <sudhansudhan83@gmail.com>
2024-03-10 13:05:43 +0530

    Displayed all entities in my database

--- clients/Checker_fuse/src/Components/listOfData.jsx
+++ clients/Checker_fuse/src/Components/listOfData.jsx
@@ -5,31 +5,19 @@ import "./Data.css";
 const ListOfData = () => {
     const [data, setData] = useState([]);
     const [error, setError] = useState(null);
-    // const [loading, setLoading] = useState(true);
-
-   
+    const url = "https://checker-fuse.onrender.com/get"
 
     const fetchData = async () => {
         try {
-            const headers = new Headers({"Access-Control-Allow-Origin":"*"});
-            const response = await fetch("http://localhost:3000/get",{
-                mode: 'no-cors',
-                headers:headers,
-                method: 'GET',
-            });
-            console.log(response)
-            const res = await response.json()
-            console.log("get", res)
-            setData(res);
-            // console.log(response)
-            setError(null); // Reset error state if fetching is successful
+            console.log("Fetching data...");
+            const response = await axios.get("http://localhost:3000/get");
+            console.log("Response:", response);
+            setData(response.data);
+            setError(null);
         } catch (error) {
             console.error("Error fetching data:", error);
             setError("Failed to fetch the data");
-        } 
-        // finally{
-        //     setLoading(false)
-        // }
+        }
     };
 
     useEffect(() => {
@@ -37,17 +25,30 @@ const ListOfData = () => {
     }, []);
 
     return (
-        <div>
-            <h3>Checker Fuse</h3>
-            {/* {loading && <p></p> } */}
-            {data.map((item, index) => (
-                <div key={index}>
-                    {/* <h3>{item.name}</h3>
-                    <h3>{item.Age}</h3> */}
-                    <h3>{item.DOB}</h3>
-                </div>
-            ))}
+        <> 
+        <div className="alignment"> 
+            <h1>Checker Fuse</h1>
+            {error ? (
+                <p>{error}</p>
+            ) : (
+                data.map((item, index) => (
+                    <div key={index} className="border">
+                        <div className="name">
+                            <h3>{item.name}</h3>
+                            <div className="identity">
+                                <h5>{item.Location}</h5>
+                                <h5>{item.Age}</h5>
+                            </div>
+                            <div className="button">
+                                <button>Loged In</button>
+                                <button>Loged Out</button>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            )}
         </div>
+        </>
     );
 };
 
