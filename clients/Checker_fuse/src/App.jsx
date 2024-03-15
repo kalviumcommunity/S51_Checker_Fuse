8e6ebfe291d9a7f968af99cae2803124ac93ed18
Sudhan1112 <sudhansudhan83@gmail.com>
2024-03-15 15:49:33 +0530

    Now I have added update features along with that I have added features selecting the status of what

--- clients/Checker_fuse/src/App.jsx
+++ clients/Checker_fuse/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ListOfData from './Components/listOfData';
 import Add from "./Components/add"
+import Update from './Components/Update';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>  
           <Route path='/' Component={ListOfData}/>
           <Route path='/add' Component={Add}/>
+          <Route path='/update/:id' Component={Update}/>
         </Routes>
       </Router>
     </div>
--- clients/Checker_fuse/src/Components/listOfData.jsx
+++ clients/Checker_fuse/src/Components/listOfData.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Data.css";
 
 const ListOfData = () => {
+    const nav = useNavigate()
     const [data, setData] = useState([]);
     const [error, setError] = useState(null);
-    const url = "https://checker-fuse.onrender.com/get"
-
+    const [color, setColor] = useState("");
+    const url = "https://checker-fuse.onrender.com/get";
     const fetchData = async () => {
         try {
             console.log("Fetching data...");
@@ -20,13 +21,35 @@ const ListOfData = () => {
             setError("Failed to fetch the data");
         }
     };
-
+    
     useEffect(() => {
         fetchData();
     }, []);
 
-    return (
-        <> 
+    const okkk = (id) => {
+        console.log(id)
+        nav(`/update/${id}`)
+    }
+    
+    const handleClick = (e, setColor) => {
+        const backGroundColor = e.target.value;
+        
+        switch(backGroundColor){
+            case "Engaged":
+                setColor("rgb(38, 224, 38)")
+                break
+                case "Serenity":
+                    setColor("gold")
+                    break
+                    case "Dormant":
+                        setColor("red")
+                        break
+                        default:
+                            setColor("")
+                        }
+                    }
+                    return (
+                        <> 
         <div className="alignment"> 
             <h1>Checker Fuse</h1>
             <div className="addBtn">
@@ -44,11 +67,16 @@ const ListOfData = () => {
                                 <h5>{item.Age}</h5>
                             </div>
                             <div className="button">
-                                <button>Engaged</button>
-                                <button>Serenity</button>
-                                <button>Dormant</button>
+                            <button value="Engaged" onClick={(e) => handleClick(e, setColor)} style={{backgroundColor: color === "rgb(38, 224, 38)" ? "rgb(38, 224, 38)" : ""}}>Engaged</button>
+                            <button value="Serenity" onClick={(e) => handleClick(e, setColor)} style={{backgroundColor: color === "gold" ? "gold" : ""}}>Serenity</button>
+                            <button value="Dormant" onClick={(e) => handleClick(e, setColor)} style={{backgroundColor: color === "red" ? "red" : ""}}>Dormant</button>
                             </div>
+                    
                         </div>
+                        <div className="btnUpdateandDelete">
+                        <button onClick={() => okkk(item.ID)} className="updt">Update</button>
+                                <button className="dlt">Delete</button>
+                            </div>
                     </div>
                 ))
             )}
