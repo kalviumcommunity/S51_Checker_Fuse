18e40b5372d29409dda90ddcce652f346d910818
Sudhan1112 <sudhansudhan83@gmail.com>
2024-03-19 14:17:30 +0530

    Updated the auth endpoints to use JWT token and stored the token in the cookie

--- Schemas/formSchema.js
+++ Schemas/formSchema.js
@@ -3,8 +3,7 @@ const Joi = require("joi");
 const formSchema = Joi.object({
     name: Joi.string().min(3).max(30).required(),
     location: Joi.string().required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-    confirmPassword: Joi.string().valid(Joi.ref('password')).required()
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required()
 })
 
 module.exports = formSchema;
--- clients/Checker_fuse/src/App.jsx
+++ clients/Checker_fuse/src/App.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import Login from './Components/Login'
-import ListOfData from './Components/listOfData';
-import Add from "./Components/add"
+import React from 'react';
+
+import ListOfData from "./Components/listOfData"
+import Add from "./Components/add";
 import Update from './Components/Update';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './Components/Login';
 
 function App() {
-
   return (
     <div>
       <Router>
         <Routes>  
-          <Route path='/' Component={Login}/>
-          <Route path='/listofentities' Component={ListOfData}/>
-          <Route path='/add' Component={Add}/>
-          <Route path='/update/:id' Component={Update}/>
+          <Route path='/' element={<Login />} />
+          <Route path='/listofentities' element={<ListOfData />} />
+          <Route path='/add' element={<Add />} />
+          <Route path='/update/:id' element={<Update />} />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
